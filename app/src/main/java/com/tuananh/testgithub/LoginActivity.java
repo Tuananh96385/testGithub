f4defe07c35cfb6217346121c3cba18667d214fd
tuananh <tuananh96385@gmail.com>
2020-08-02 21:02:55 +0700

    commit changed

---
+++ app/src/main/java/com/tuananh/testgithub/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.tuananh.testgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/tuananh/testgithub/Student.java
@@ -0,0 +1,4 @@
+package com.tuananh.testgithub;
+
+public class Student {
+}
